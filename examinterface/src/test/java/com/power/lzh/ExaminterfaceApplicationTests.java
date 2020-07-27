10fc5b4b6a4f5ac0906e3653e261be60f6bc8880
hui0930 <1907361806@qq.com>
2020-07-27 21:06:05 +0800

    springboot考试星接口

---
+++ examinterface/src/test/java/com/power/lzh/ExaminterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.power.lzh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
